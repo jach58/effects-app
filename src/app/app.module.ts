609a8d22308befd1272e308ad1d2a6067bc4d6ce
Juan Castillo <jach58@hotmail.com>
2018-12-08 14:11:56 -0600

    Primer commit app sin effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    UsuariosModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
